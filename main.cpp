7d363ed240e456e3525ed09a09e7b4b386c59cdc
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2022-11-28 18:43:49 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+Proyecto Kerbal Space Program 
+Ramiro Flores Villarreal
+A01710879
+main.cpp
+*/
+
+#include <iostream>
+#include <string>
+#include "ft-liquid.h"
+#include "ft-rcs.h"
+#include "ft-rocket.h"
+#include "e-ionfuel.h"
+#include "e-liquidfuel.h"
+#include "e-solidfuel.h"
+#include "Pods.h"
+
+using namespace std;
+
+int main(){
+
+}
